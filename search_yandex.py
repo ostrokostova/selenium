0957b213ced3539a0aacb7682ee88ee8d0994d3d
natasha <ostrokostova@rb.asu.ru>
2021-11-12 23:42:02 +0700

    add search yandex

---
+++ search_yandex.py
@@ -0,0 +1,12 @@
+import time # подключение модуля для работы со временем
+from selenium import webdriver # подключение webdriver для управления браузером
+driver = webdriver.Chrome()# инициализируется драйвер браузера. После этой команды должно открыться новое окно браузера
+time.sleep(5) # команда time.sleep устанавливает паузу в 5 секунд, чтобы успеть увидеть, что происходит в браузере
+driver.get("https://ya.ru")# Метод get сообщает браузеру, что нужно открыть сайт по указанной ссылке
+time.sleep(5)
+searche_input = driver.find_element_by_id("text")
+time.sleep(5)
+searche_input.send_keys("АГУ")
+button_input = driver.find_element_by_tag_name("form").submit()
+time.sleep(5)
+driver.quit()# После выполнения всех действий надо закрыть окно браузера
